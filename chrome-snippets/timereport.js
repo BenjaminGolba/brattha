b03c85c7a3c30883ced60cf26151bf08bc033dd3
BenjaminGolba <4691048+BenjaminGolba@users.noreply.github.com>
2024-11-29 09:45:25 +0100

    Update timereport.js

--- chrome-snippets/timereport.js
+++ chrome-snippets/timereport.js
@@ -1,7 +1,24 @@
+/**
+ * INSTRUCTIONS
+ *
+ * 0. Define ASSIGNMENT_ID and TIME_TYPE_ID below
+ * 
+ * 1. Go to the time reporting site
+ * 2. Select the calendar view "Månad"
+ * 3. Press "Välj flera"
+ * 4. Select the dates you want to time report
+ * 5. When all desired dates are selected, press Ny tidrapport
+ *   -> Modal should open
+ * 6. Run the script
+ *   -> Refresh the page and dates should be populated
+ *
+ * 7. Now adjust the dates that deviate from the "standard" time manually
+ */
+
 const DEFAULT_START_HOUR = "08:00";
 const DEFAULT_END_HOUR = "16:00";
 
-/*
+/**
  * To find these two, go to the calendar view, open devtools and 
  * look for the data-fields below and insert them into the const.
  * Example: const ASSIGMENT_ID = "123123-123123-123123-123123";
